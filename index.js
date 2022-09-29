6cb515e77e4437016c1ff43f9e2751dc1da00de6
HeyArnoldo <joao.souza@colegiounion.edu.pe>
2022-09-28 23:42:13 -0500

    Some edits

--- index.js
+++ index.js
@@ -13,31 +13,26 @@ class InstagramClient {
     console.log("Followers: " + userInfo.follower_count);
     console.log("Following: " + userInfo.following_count);
 
-    const getWaitTime = () => (Math.random() * 5 * 60000) - (Math.random() * 1 * 60000);
-    console.log("Waiting for " + (getWaitTime() / 1000)  + " seg");
+    //const getWaitTime = () => (Math.random() * 5 * 60000) - (Math.random() * 1 * 60000);
+    //console.log("Waiting for " + (getWaitTime() / 1000)  + " seg");
 
     setInterval(async() => {
     try{
       const pendingFollows = await this.getPendingFollows();
       if(pendingFollows.length > 0) {
         pendingFollows.forEach(async (user) => {
+          const { follower_count, following_count } = this.ig.user.info(this.ig.state.cookieUserId);
           this.ig.friendship.approve(user.pk).then(res => console.log("Pending Follow Accepted: " + user.username));
-          await bots.follow(user.username).then(res => console.log("Follow backed: " + user.username));
+          await bots.follow(user.username).then(res => console.log("Followers: " + follower_count + " Following: " + following_count));
         });
       }
     }catch(e){console.log(e)}
-  }, getWaitTime());
+  }, 60000);
   }
 
   async login() {
     this.ig.state.generateDevice(config.instagram.username);
     await this.ig.account.login(config.instagram.username, config.instagram.password);
-  }
-  
-  async follow(username) {
-    const id = await this.ig.user.getIdByUsername(username)
-    console.log("Following: " + username + " ,with id: " + id);
-    await this.ig.friendship.create(id);
   }
   
   async getPendingFollows() {
